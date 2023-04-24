4fbfdd55e79151941b05838606e3d9ad9f9ddf84
José Hurtado <jose_hurtado@outlook.com>
2023-04-24 17:05:41 -0500

    UPDATE: actualizacion en codigo

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,14 +1,12 @@
 import express from "express";
-const router = express.Router();
 import {
   registrar,
   autenticar,
   perfil,
 } from "../controllers/usuarioController.js";
-
 import checkAuth from "../middleware/checkAuth.js";
 import { autenticarValidation, registrarValidation } from '../validations/validationAuth.js'
-
+const router = express.Router();
 router.post("/", registrarValidation, registrar);
 router.post("/login", autenticarValidation, autenticar);
 router.get("/perfil", checkAuth, perfil);
